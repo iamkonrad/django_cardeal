a48be7f968101b336011fc1e9957b1e1646a4f41
iamkonrad <133384502+iamkonrad@users.noreply.github.com>
2023-09-23 21:40:46 +0200

    Cars app models
    creation of choices.py file inside cars app, inside cars models creation of the relevant fields, such as name, country, photo etc., importing status choices for prodution_year,
    doors_choices, features_choices, registering models in admin, adding ckeditor for a custom RichTextField description field, installing it in settings.py and importing into cars/models and description field

---
+++ django_cardeal/cars/choices.py
@@ -0,0 +1,32 @@
+from datetime import datetime
+
+
+Doors_Choices = (
+    ('2', '2'),
+    ('3', '3'),
+    ('4', '4'),
+    ('5', '5'),
+    ('6', '6'),
+
+    )
+
+Features_Choices = (
+    ('Cruise Control', 'Cruise Control'),
+    ('Audio Interface', 'Audio Interface'),
+    ('Airbags', 'Airbags'),
+    ('Air Conditioning', 'Air Conditioning'),
+    ('Seat Heating', 'Seat Heating'),
+    ('Alarm System', 'Alarm System'),
+    ('ParkAssist', 'ParkAssist'),
+    ('Power Steering', 'Power Steering'),
+    ('Reversing Camera', 'Reversing Camera'),
+    ('Direct Fuel Injection', 'Direct Fuel Injection'),
+    ('Auto Start/Stop', 'Auto Start/Stop'),
+    ('Wind Deflector', 'Wind Deflector'),
+    ('Bluetooth Handset', 'Bluetooth Handset'),
+    )
+
+
+Year_Choices =[]
+for r in range(1900,(datetime.now().year+1)):
+    Year_Choices.append((r,r))
--- django_cardeal/cars/models.py
+++ django_cardeal/cars/models.py
@@ -1,3 +1,37 @@
 from django.db import models
+from datetime import datetime
+from django_countries.fields import CountryField
+from ckeditor.fields import RichTextField
+from .choices import Year_Choices, Features_Choices, Doors_Choices
 
-# Create your models here.
+
+
+class Car(models.Model):
+    car_name = models.CharField(max_length=500)
+    country= CountryField(blank_label='(select a country)')
+    province = models.CharField(max_length=200, blank=True, null=True)
+    city = models.CharField(max_length=200)
+    color = models.CharField(max_length=200)
+    model = models.CharField(max_length=200)
+    production_year = models.IntegerField(('year'),choices=Year_Choices)
+    condition = models.CharField(max_length=200)
+    price = models.IntegerField()
+    description = RichTextField()
+    car_photo= models.ImageField(upload_to='photos/%Y/%m/%d/')
+    car_photo_1= models.ImageField(upload_to='photos/%Y/%m/%d/',blank=True, null=True)
+    car_photo_2 = models.ImageField(upload_to='photos/%Y/%m/%d/',blank=True, null=True)
+    car_photo_3 = models.ImageField(upload_to='photos/%Y/%m/%d/',blank=True, null=True)
+    car_photo_4 = models.ImageField(upload_to='photos/%Y/%m/%d/',blank=True, null=True)
+    features = models.CharField(choices=Features_Choices, max_length=200)
+    body_style = models.CharField(max_length=200)
+    engine = models.CharField(max_length=200)
+    transmission = models.CharField(max_length=200)
+    interior = models.CharField(max_length=200)
+    mileage = models.IntegerField()
+    doors = models.CharField(choices=Doors_Choices,max_length=20)
+    passengers = models.IntegerField()
+    vin_no = models.CharField(max_length=200)
+    fuel_type = models.CharField(max_length=200)
+    number_of_owners = models.CharField(max_length=200)
+    is_featured = models.BooleanField(default=False)
+    created_date = models.DateTimeField(default=datetime.now,blank=True)
