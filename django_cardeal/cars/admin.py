8776fc0597610bbec604a547e7943f49c68f2dd6
iamkonrad <133384502+iamkonrad@users.noreply.github.com>
2023-09-24 20:16:59 +0200

    customizing cars app admin panel
    inside cars app admin.py adding a list_dsplay variable that will allow for display of additional db categories inside the admin panel, such as car_name, model and so on; also
    adding a thumbnail method to the CarAdmin class object that will allow for the display of car photos inside the admin panel, also making some fields clickable, such as photos and editable, such as is_editable,
    finally adding an admin panel search-bar

--- django_cardeal/cars/admin.py
+++ django_cardeal/cars/admin.py
@@ -1,5 +1,17 @@
 from django.contrib import admin
+from django.utils.html import format_html
+
 from .models import Car
 
 
-admin.site.register(Car)
+class CarAdmin(admin.ModelAdmin):
+    def thumbnail(self,object):
+        return format_html('<img src="{}" width="40" style="border-radius:50px;" />'. format(object.car_photo.url))
+    thumbnail.short_description = 'Car Image'
+
+    list_display=('id','thumbnail','car_name','city','color','model','production_year','fuel_type','is_featured')
+    list_display_links= ('id','thumbnail','car_name')
+    list_editable=('is_featured',)
+    search_fields=('id','car_name','city','model','body_style','fuel_type')
+    list_filter=('city','model','fuel_type')
+admin.site.register(Car,CarAdmin)
