391290dce6785de604f186a0a31062174f225686
iamkonrad <133384502+iamkonrad@users.noreply.github.com>
2023-09-28 20:39:28 +0200

    Search functionality for the cars.html page
    creation of a fully responsive and dynamically generated search form for cars.html; inside cars app views creating variables: model_search, city_search
    year_search and body_style_search that will fetch their corresponding objects from the db and assure the uniquenes of the search and passing them into cars function context dictionary; inside cars views
    adding model_search, city_search, year_search and body_style_search and passing them into search function via context dictionary, inside search.html, styling the forms to conform to the db requirements, inside
    search.html creation of a for model in model_search loop, adding for loops for other categories of data that will populate the search forms inside of search.html

--- django_cardeal/cars/views.py
+++ django_cardeal/cars/views.py
@@ -8,8 +8,17 @@ def cars(request):
     paginator = Paginator(cars,4)
     page = request.GET.get('page')
     paged_cars = paginator.get_page(page)
+    model_search=Car.objects.values_list('model',flat=True).distinct()
+    city_search=Car.objects.values_list('city',flat=True).distinct()
+    year_search=Car.objects.values_list('production_year',flat=True).distinct()
+    body_style_search=Car.objects.values_list('body_style',flat=True).distinct()
+
     data = {
         'cars':paged_cars,
+        'model_search': model_search,
+        'city_search': city_search,
+        'year_search': year_search,
+        'body_style_search': body_style_search,
     }
     return render(request,'cars/cars.html',data)
 
@@ -23,6 +32,13 @@ def car_detail(request,id):
 
 def search (request):
     cars = Car.objects.order_by('-created_date')
+
+    model_search=Car.objects.values_list('model',flat=True).distinct()
+    city_search=Car.objects.values_list('city',flat=True).distinct()
+    year_search=Car.objects.values_list('production_year',flat=True).distinct()
+    body_style_search=Car.objects.values_list('body_style',flat=True).distinct()
+    transmission_search=Car.objects.values_list('transmission',flat=True).distinct()
+
 
     if 'keyword' in request.GET:
         keyword = request.GET['keyword']
@@ -59,6 +75,12 @@ def search (request):
 
     data = {
         'cars':cars,
+        'model_search': model_search,
+        'city_search': city_search,
+        'year_search': year_search,
+        'body_style_search': body_style_search,
+        'transmission_search': transmission_search,
+
     }
 
     return render (request,'cars/search.html',data)
