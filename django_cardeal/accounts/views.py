62f4548569d3baf982dc4c5adf2689d4d62588ca
iamkonrad <133384502+iamkonrad@users.noreply.github.com>
2023-09-29 19:06:10 +0200

    Enabling Django messages
    Adding django messages dictionary inside the settings.py; inside root templates/includes creation of messages.html file and inclusion of for message in messages for
    loop therein that will loop over all the messages and create divs to display them, adding an if check to determine the type of message shown, i.e. if it's an error, an error class message will be displayed;
    importing messages.html inside the register.html; inside static/app.js setting the timer for the messages display

--- django_cardeal/accounts/views.py
+++ django_cardeal/accounts/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect
+from django.contrib import messages
 
 
 def login(request):
@@ -8,7 +9,7 @@ def logout(request):
     return redirect('home')
 
 def register(request):
-    return render(request,'accounts/register.html')
+        return render(request,'accounts/register.html')
 
 def dashboard(request):
     return render(request,'accounts/dashboard.html')
