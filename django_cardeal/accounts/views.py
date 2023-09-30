fcea244bb1ed07b59e003984d88f0bec78bac3cc
iamkonrad <133384502+iamkonrad@users.noreply.github.com>
2023-09-30 19:57:03 +0200

    Registration functionality
    Inside dashboard.html loading the django messages; Inside accounts app views register function creating an in check to check whether the data was submitted via POST method, if it was
    not a user will be redirected to register page; inside the previous if check nesting a check for assuring that password is equal to confirm_password in the the register form, in case the passwords match, the
    check will assure that there is no email associated with paricular user in the db or that a particular user doesn't already exist in the db, in case new username meets teh requirements newly created user will be
    redirected to the dashboard, in case new username doesn't meet the requirements the user will be redirected to a registration page, proper error and success messages will display

--- django_cardeal/accounts/views.py
+++ django_cardeal/accounts/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
-from django.contrib import messages
+from django.contrib import messages,auth
+from django.contrib.auth.models import User
 
 
 def login(request):
@@ -9,6 +10,33 @@ def logout(request):
     return redirect('home')
 
 def register(request):
+    if request.method == 'POST':
+        firstname = request.POST['firstname']
+        lastname = request.POST['lastname']
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        confirm_password = request.POST['confirm_password']
+
+        if password == confirm_password:
+            if User.objects.filter(username=username).exists():
+                messages.error(request,'Username already exists')
+                return redirect('register')
+            else:
+                if User.objects.filter(email=email).exists:
+                    messages.error(request, 'Email already exists in the db')
+                    return redirect ('register')
+                else:
+                    user = User.objects.create_user(first_name=firstname, last_name=lastname, email=email, username=username, password=password)
+                    auth.login(request,user)
+                    messages.success(request,'Your account has been created. You are now logged-in')
+                    return redirect('dashboard')
+
+        else:
+            messages.error(request, 'Passwords do not match')
+            return redirect('register')
+
+    else:
         return render(request,'accounts/register.html')
 
 def dashboard(request):
